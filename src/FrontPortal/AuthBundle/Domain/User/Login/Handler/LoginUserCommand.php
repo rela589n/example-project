188eef333b6ff4c1e0702c9ce7984299e6a8b4f7
Yevhen Sidelnyk <zsidelnik@gmail.com>
2024-08-20 09:12:47 +0300

    wip

--- src/FrontPortal/AuthBundle/Domain/User/Login/Handler/LoginUserCommand.php
+++ src/FrontPortal/AuthBundle/Domain/User/Login/Handler/LoginUserCommand.php
@@ -7,22 +7,30 @@ namespace App\FrontPortal\AuthBundle\Domain\User\Login\Handler;
 use App\FrontPortal\AuthBundle\Domain\User\Exception\UserNotFoundException;
 use App\FrontPortal\AuthBundle\Domain\User\Login\Exception\PasswordMismatchException;
 use App\FrontPortal\AuthBundle\Domain\ValueObject\Email\EmailValidationFailedException;
+use PhPhD\ExceptionalValidation;
 use PhPhD\ExceptionalValidation\Capture;
 use PhPhD\ExceptionalValidation\Formatter\ViolationListExceptionFormatter;
 use PhPhD\ExceptionalValidation\Model\Condition\ValueExceptionMatchCondition;
 use SensitiveParameter;
 
+#[ExceptionalValidation]
 final readonly class LoginUserCommand
 {
     public function __construct(
-        #[Capture(
+        #[ExceptionalValidation\Capture(
             exception: EmailValidationFailedException::class,
             condition: ValueExceptionMatchCondition::class,
             formatter: ViolationListExceptionFormatter::class,
         )]
-        #[Capture(exception: UserNotFoundException::class, condition: ValueExceptionMatchCondition::class)]
+        #[ExceptionalValidation\Capture(
+            exception: UserNotFoundException::class,
+            condition: ValueExceptionMatchCondition::class,
+        )]
         private string $email,
-        #[Capture(exception: PasswordMismatchException::class, condition: ValueExceptionMatchCondition::class)]
+        #[ExceptionalValidation\Capture(
+            exception: PasswordMismatchException::class,
+            condition: ValueExceptionMatchCondition::class,
+        )]
         #[SensitiveParameter]
         private string $password,
     ) {
