92c855472ff96bfa87510f459f720f63e5f78a9e
Yevhen Sidelnyk <zsidelnik@gmail.com>
2024-09-20 11:34:23 +0300

    wip

---
+++ src/Common/PlaygroundBundle/Domain/Sleep/Api/SleepFrontendAction.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\PlaygroundBundle\Domain\Sleep\Api;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[AsController]
+final readonly class SleepFrontendAction
+{
+    #[Route('/sleep')]
+    public function __invoke(
+        #[MapQueryParameter('time')]
+        int $sleepTime = 0,
+    ): Response {
+        sleep($sleepTime);
+
+        return new Response('Yawn');
+    }
+}
