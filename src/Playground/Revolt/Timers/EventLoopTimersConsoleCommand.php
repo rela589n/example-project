2ba80e2e8e1ec3bdae76f29fbdae5c2cff801490
Yevhen Sidelnyk <zsidelnik@gmail.com>
2024-10-30 16:07:41 +0200

    wip

---
+++ src/Playground/Revolt/Timers/EventLoopTimersConsoleCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Playground\Revolt\Timers;
+
+use Revolt\EventLoop;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand('app:revolt:timers')]
+final class EventLoopTimersConsoleCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        EventLoop::queue(
+            static function () use ($output) {
+                // repeated callbacks are started after the interval period has elapsed
+                $repeatCallbackId = EventLoop::repeat(1, static function () use ($output) {
+                    $output->writeln('Repeat');
+                });
+
+                // delayed callbacks are executed after the delay time has elapsed
+                EventLoop::delay(5, static function () use ($output, $repeatCallbackId) {
+                    EventLoop::cancel($repeatCallbackId);
+
+                    EventLoop::defer(static function () use ($output) {
+                        $output->writeln('Loop completed (last tick)');
+                    });
+
+                    EventLoop::queue(static function () use ($output) {
+                        $output->writeln('Loop finished');
+                    });
+                });
+
+                // deferred callbacks are executed on the next tick
+                EventLoop::defer(static function () use ($output) {
+                    $output->writeln('Loop started');
+                });
+
+                // queued callbacks are executed in the current tick
+                EventLoop::queue(static function () use ($output) {
+                    $output->writeln('Start loop');
+                });
+            },
+        );
+
+        EventLoop::run();
+
+        return 0;
+    }
+}
