d995f11e0232cf487e3f24005aa145c0f2960af6
Yevhen Sidelnyk <zsidelnik@gmail.com>
2025-05-02 14:31:58 +0300

    use framework serializer configuration instead of custom RequestPayloadSerializer

--- src/Support/Api/Bundle/AppApiBundle.php
+++ src/Support/Api/Bundle/AppApiBundle.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace App\Support\Api\Bundle;
 
 use App\Support\Api\Bundle\DependencyInjection\AppApiExtension;
-use App\Support\Api\Bundle\RequestMapping\Serializer\RequestPayloadSerializerCompilerPass;
 use Override;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AppApiBundle extends Bundle
@@ -16,13 +14,5 @@ final class AppApiBundle extends Bundle
     protected function createContainerExtension(): AppApiExtension
     {
         return new AppApiExtension();
-    }
-
-    #[Override]
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new RequestPayloadSerializerCompilerPass());
     }
 }
