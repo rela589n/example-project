03fb9c9e5aa46617abb5fa26673474f097e65dcc
Yevhen Sidelnyk <zsidelnik@gmail.com>
2025-07-29 15:37:44 +0300

    add Override attribute

--- src/Support/CycleBridge/Bundle/AppCycleBridgeBundle.php
+++ src/Support/CycleBridge/Bundle/AppCycleBridgeBundle.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Support\CycleBridge\Bundle;
 
+use App\Support\CycleBridge\Bundle\DependencyInjection\AppCycleBridgeExtension;
+use Override;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AppCycleBridgeBundle extends Bundle
 {
+    #[Override]
+    protected function createContainerExtension(): ?AppCycleBridgeExtension
+    {
+        return new AppCycleBridgeExtension();
+    }
 }
--- src/Support/MessageBus/Bundle/AppMessageBusBundle.php
+++ src/Support/MessageBus/Bundle/AppMessageBusBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Support\MessageBus\Bundle;
 
+use App\Support\MessageBus\Bundle\DependencyInjection\AppMessageBusExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AppMessageBusBundle extends Bundle
 {
+    #[Override]
+    protected function createContainerExtension(): AppMessageBusExtension
+    {
+        return new AppMessageBusExtension();
+    }
 }
